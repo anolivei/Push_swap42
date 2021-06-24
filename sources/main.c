837732800ce1a344a72aad5dd683b0f432556f9a
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-06-23 23:53:02 -0300

    atoi

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 19:07:27 by anolivei          #+#    #+#             */
-/*   Updated: 2021/06/13 16:24:38 by anolivei         ###   ########.fr       */
+/*   Updated: 2021/06/23 23:07:59 by anolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 static void	error(void)
 {
 	ft_putstr_fd("Error\n", 1);
+	exit(1);
 }
 
-static void	validate_args(int argc, char **argv)
+static int	validate_args(int argc, char **argv)
 {
 	int	i;
 
@@ -25,15 +26,46 @@ static void	validate_args(int argc, char **argv)
 	while (i < argc)
 	{
 		if (is_integer(argv[i]) == 0)
+		{
 			error();
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+static void	transform_args(int argc, char **argv, int *num)
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		num[i] = ft_atoi(argv[i]);
 		i++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
+	int	i;
+	int	*num;
+
 	argc--;
 	argv++;
+	num = malloc(sizeof(int) * argc);
+	if (!num)
+		return(false);
 	validate_args(argc, argv);
-	return (0);
+	transform_args(argc, argv, num);
+	i = 0;
+	while (i < argc)
+	{
+		printf("%i, ", num[i]);
+		i++;
+	}
+	free(num);
+	num = NULL;
+	return (true);
 }
